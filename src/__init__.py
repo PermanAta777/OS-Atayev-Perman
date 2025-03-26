14a61529ef0010752609b0b7e11341bcae3e3b36
PermanAta777 <permanatabgu@gmail.com>
2025-03-26 12:21:13 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Пакет для работы с числами Фибоначчи, палиндромами и связными списками
+"""
+
+from .fibonacci import FibonacciGenerator
+from .palindrome import PalindromeChecker
+from .linked_list import LinkedList
+
+__all__ = ['FibonacciGenerator', 'PalindromeChecker', 'LinkedList']
